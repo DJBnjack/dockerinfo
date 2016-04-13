7339346315d59579a990b0693c8fa8f2a875750e
Johannes <DJBnjack@users.noreply.github.com>
2016-04-13 21:51:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def info():
     outstr =  '<pre>'
     for key in sorted(os.environ.keys()):
-        outstr += "%-50s %s \n" % (key,os.environ[key])
+        outstr += "%30s %s \n" % (key,os.environ[key])
     return outstr + '</pre>'
 
 if __name__ == '__main__':
